b1eec833e70b6ac574941b987a8664a95c46ff2c
firosmalik000 <firosmalik44@gmail.com>
2024-09-05 22:35:04 +0700

    add event to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
+
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="relative z-[100] bg-blue-800 h-[70px] w-full px-10  ">
-      <div className="h-full flex items-center justify-between text-white ">
+    <div className={`fixed top-0 z-[100] h-[70px] w-full px-10 transition-colors duration-300 ${isScrolled ? 'bg-white shadow-xl text-black' : 'bg-blue-800 text-white'}`}>
+      <div className="h-full flex items-center justify-between ">
         <div className="text-3xl">Kevin Shipping</div>
         <div className="flex gap-x-4">
           <div>About Us</div>
